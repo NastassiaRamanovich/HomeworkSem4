a64fa2908aa4e7964bcb82d7ec09cd5f9facff0e
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-08-31 23:45:24 +0300

    Нахождение суммы цифр числа

---
+++ Sem4_Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход 
+// число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+if (n < 0)
+{
+    n = -1 * n;
+}
+while (n > 0)
+{
+    sum = sum + n % 10;
+    n = (n - n % 10) / 10;
+}
+Console.WriteLine(sum);
