30d90351ddc76ebe6a48aab364bfe064c41e13a8
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-09-01 21:07:31 +0300

    Изменения

--- Sem6_Task39/Program.cs
+++ Sem6_Task39/Program.cs
@@ -4,33 +4,6 @@
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
 
-int [] array = new int[5];
-
-void fillArray(int[] array)
-{ //void ничего не возвращает
-  for (int i = 0; i < array.Length; i++)
-  {
-        array[i] = new Random().Next(1, 9);       
-  }
-//   Console.WriteLine("[{0}]", string.Join(", ", array));
-}
-
-void Reverse (int[] array)
-{
-  int temp;
-  for (int i = 0; i < array.Length / 2; i++)
-  {
-        temp = array[i];
-        array[i] = array[array.Length - 1 - i];
-        array[array.Length - 1 - i] = temp;       
-  }
-}
-
-fillArray(array);
-Console.WriteLine("[{0}]", string.Join(", ", array));
-Reverse(array);
-Console.WriteLine("[{0}]", string.Join(", ", array));
-
 // int [] array = new int[5];
 
 // void fillArray(int[] array)
@@ -42,18 +15,45 @@ Console.WriteLine("[{0}]", string.Join(", ", array));
 // //   Console.WriteLine("[{0}]", string.Join(", ", array));
 // }
 
-// int[] ReverseNew (int[] array)
+// void Reverse (int[] array)
 // {
-//   int newArray = new int[array.Length];
+//   int temp;
 //   for (int i = 0; i < array.Length / 2; i++)
 //   {
-//         newArray[array.Length - 1 - i] = array[i];       
+//         temp = array[i];
+//         array[i] = array[array.Length - 1 - i];
+//         array[array.Length - 1 - i] = temp;       
 //   }
-//   return newArray;
 // }
 
 // fillArray(array);
 // Console.WriteLine("[{0}]", string.Join(", ", array));
-// Console.WriteLine("[{0}]", string.Join(", ", ReverseNew(array));
 // Reverse(array);
 // Console.WriteLine("[{0}]", string.Join(", ", array));
+
+int [] array = new int[5];
+
+void fillArray(int[] array)
+{ //void ничего не возвращает
+  for (int i = 0; i < array.Length; i++)
+  {
+        array[i] = new Random().Next(1, 9);       
+  }
+//   Console.WriteLine("[{0}]", string.Join(", ", array));
+}
+
+int[] ReverseNew (int[] array)
+{
+  int newArray = new int[array.Length];
+  for (int i = 0; i < array.Length / 2; i++)
+  {
+        newArray[array.Length - 1 - i] = array[i];       
+  }
+  return newArray;
+}
+
+fillArray(array);
+Console.WriteLine("[{0}]", string.Join(", ", array));
+Console.WriteLine("[{0}]", string.Join(", ", ReverseNew(array));
+Reverse(array);
+Console.WriteLine("[{0}]", string.Join(", ", array));
